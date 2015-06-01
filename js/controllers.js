94252fea0d603d1c93957d58c7ff5e6f19459662
VoodooSL <monkey.wonder@voodoo-sl.com>
2015-06-02 00:40:13 +0100

    no message

---
+++ js/controllers.js
@@ -0,0 +1,20 @@
+﻿angular.module('voodooMobile', [])
+.controller('navController', ['$scope', '$rootScope', function ($scope, $rootScope) {
+    $scope.showNav = false;
+}])
+.controller('loginController', ['$scope', '$rootScope', function ($scope, $rootScope) {
+    $scope.showLogin = false;
+    $scope.email = '';
+    $scope.password = '';
+
+    function init() {
+        if (!localStorage.loginDetails)
+            $scope.showLogin = true;
+    };
+
+    $scope.login = function (email, password) {
+        alert(email);
+    };
+
+    init();
+}]);
