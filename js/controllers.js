33e56ec36d38bfb29a3174b3a530d35e6266dca1
VoodooSL <monkey.wonder@voodoo-sl.com>
2015-06-02 01:40:53 +0100

    no message

--- js/controllers.js
+++ js/controllers.js
@@ -1,19 +1,44 @@
-﻿angular.module('voodooMobile', [])
+﻿angular.module('voodooMobileControllers', [])
 .controller('navController', ['$scope', '$rootScope', function ($scope, $rootScope) {
     $scope.showNav = false;
 }])
+.controller('socialController', ['$scope', '$rootScope', function ($scope, $rootScope) {
+
+}])
 .controller('loginController', ['$scope', '$rootScope', function ($scope, $rootScope) {
     $scope.showLogin = false;
-    $scope.email = '';
-    $scope.password = '';
+    $scope.inputEmail = '';
+    $scope.inputPassword = '';
+    $scope.loginOffset = 0;
+    $scope.emailInvalid = false;
+    $scope.passwordInvalid = false;
 
     function init() {
-        if (!localStorage.loginDetails)
+        if (!localStorage.loginDetails) {
             $scope.showLogin = true;
+            $scope.loginOffset = 'margin-top:' + (((window.innerHeight - $('#loginModal').prev().height()) / 2) - ($('#loginModal').height() / 2)) + 'px;';
+        }
     };
 
-    $scope.login = function (email, password) {
-        alert(email);
+    $scope.login = function () {
+        var isOk = true;
+        if ($scope.inputPassword == '')
+        {
+            $('#inputPassword').focus();
+            $scope.passwordInvalid = true;
+            isOk = false;
+        }
+        else
+            $scope.passwordInvalid = false;
+
+        if ($scope.inputEmail == '') {
+            $('#inputEmail').focus();
+            $scope.emailInvalid = true;
+            isOk = false;
+        }
+        else
+            $scope.emailInvalid = false;
+
     };
 
     init();
---
+++ js/main.js
@@ -0,0 +1,15 @@
+﻿angular.module('voodooMobile', ['ngRoute', 'voodooMobileControllers'])
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+         when('/start', {
+             templateUrl: 'start.html',
+             controller: 'loginController'
+         }).
+         when('/social', {
+             templateUrl: 'social.html',
+             controller: 'socialController'
+         }).
+         otherwise({
+             redirectTo: '/start'
+         });
+}]);
